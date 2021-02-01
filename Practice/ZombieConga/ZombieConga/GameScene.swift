2893d7bd9c77c581f842f1c30746346ab09868c9
PetterBraka <petter.braka@gmail.com>
2021-02-01 11:06:46 +0000

    Added the background and some example assets but I need to add the proper assets for the project

--- Practice/ZombieConga/ZombieConga/GameScene.swift
+++ Practice/ZombieConga/ZombieConga/GameScene.swift
@@ -10,5 +10,13 @@ import SpriteKit
 class GameScene: SKScene {
     override func didMove(to view: SKView){
         backgroundColor = SKColor.black
+        let background = SKSpriteNode(imageNamed: "background")
+        addChild(background)
+        background.position = CGPoint(x: size.width/2, y: size.height/2)
+        background.scale(to: CGSize(width: 2048 * 2.4, height: 640 * 2.4))
+        background.anchorPoint = .init(x: 0.5, y: 0.5) // Default anchor
+//        background.zRotation = CGFloat.pi / 8
+        let mySize = background.size
+        print("Size: \(mySize)")
     }
 }
