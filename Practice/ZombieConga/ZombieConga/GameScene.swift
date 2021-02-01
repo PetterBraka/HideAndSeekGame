ed26da602ea8fb3133dac37a2157a32e6ac9fe28
PetterBraka <petter.braka@gmail.com>
2021-02-01 09:53:43 +0000

    created the first practice project for game dev.

---
+++ Practice/ZombieConga/ZombieConga/GameScene.swift
@@ -0,0 +1,14 @@
+//
+//  GameScene.swift
+//  ZombieConga
+//
+//  Created by Petter vang Brakalsvålet on 01/02/2021.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMove(to view: SKView){
+        backgroundColor = SKColor.black
+    }
+}
