927dcdd4c7c2a482219d011217e8b2e64151e1ca
PetterBraka <petter.braka@gmail.com>
2021-03-21 13:18:49 +0000

    Created a class for hidingSpots

---
+++ Game/HideAndSeek/HideAndSeek/hidingSpot.swift
@@ -0,0 +1,64 @@
+//
+//  HidingSpot.swift
+//  HideAndSeek
+//
+//  Created by Petter vang Brakalsvålet on 21/03/2021.
+//
+
+import UIKit
+import SpriteKit
+
+class HidingSpot: NSObject {
+    enum Variant: String {
+        case mountan = "mountan"
+        case house = "house"
+        case tent = "tent"
+        case tree = "tree"
+        case lake = "lake"
+    }
+    
+    private let zPosition = 2
+    private var size: CGSize = CGSize(width: 20, height: 20)
+    
+    let type: Variant
+    let location: CGPoint
+    let capacity: Int?
+    
+    var image: String
+    var avalible: Bool?
+    
+    init(_ variant: Variant, _ location: CGPoint, _ capacity: Int?, image: String, avalible: Bool?) {
+        self.type = variant
+        self.location = location
+        self.image = image
+        self.avalible = avalible
+        self.capacity = capacity
+        super.init()
+//        Warning - Check if this will work (Will it run the function getSize()?)
+        self.size = getSize()
+    }
+    
+    private func getSize() -> CGSize {
+        switch type {
+        case .mountan:
+            return CGSize(width: 100, height: 400)
+        case .house:
+            return CGSize(width: 125, height: 125)
+        case .tent:
+            return CGSize(width: 30, height: 30)
+        case .tree:
+            return CGSize(width: 20, height: 60)
+        case .lake:
+            return CGSize(width: 200, height: 75)
+        }
+    }
+    
+    func createSprite() -> SKSpriteNode {
+        let place = SKSpriteNode(imageNamed: image)
+        place.position = location
+        place.name = type.rawValue
+        place.aspectFillToSize(size: size)
+        return place
+    }
+    
+}
