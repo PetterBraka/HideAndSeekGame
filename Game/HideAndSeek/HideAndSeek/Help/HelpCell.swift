5bd42d7b97ce7208eb64b66ec08077d16e6262fd
PetterBraka <petter.braka@gmail.com>
2021-04-25 12:46:02 +0100

    refactored Help files

--- Game/HideAndSeek/HideAndSeek/Help/HelpCell.swift
+++ Game/HideAndSeek/HideAndSeek/Help/HelpCell.swift
@@ -11,24 +11,15 @@ class HelpCell: UITableViewCell {
     @IBOutlet weak var cellImage: UIButton!
     @IBOutlet weak var label: UILabel!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-    
+    // This will populate the cell with the data passed.
     func populate(_ item: HelpVC.HelpItems) {
         cellImage.setBackgroundImage(UIImage(named: item.image), for:.normal)
-        if item.secondImage != nil{
-            cellImage.setImage(UIImage(named: item.secondImage!), for: .normal)
-        } else {
-            cellImage.setImage( .none, for: .normal)
-        }
         label.text = item.explenation
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        // Checks if a second image is passed, and if not it will set a blank image as the image.
+        guard let secondImage = item.secondImage else {
+            cellImage.setImage( .none, for: .normal)
+            return
+        }
+        cellImage.setImage(UIImage(named: secondImage), for: .normal)
     }
 }
--- Game/HideAndSeek/HideAndSeek/Help/HelpVC.swift
+++ Game/HideAndSeek/HideAndSeek/Help/HelpVC.swift
@@ -19,21 +19,25 @@ class HelpVC: UIViewController {
         var explenation: String
     }
     
+    //Creates and array of preset help options
     var helpItems = [HelpItems(image: "Hider", explenation: "This is a hider. Your job as a hider is to hide from the seeker."),
-                     HelpItems(image: "Seeker", explenation: "This is a seeker. Your job as a seeker is to catch all other players"),
-                     HelpItems(image: "tentNew", explenation: "This is an hiding spot. You can hide here"),
-                     HelpItems(image: "tentOld", explenation: "This is an hiding spot. You can hide here"),
-                     HelpItems(image: "house", explenation: "This is an hiding spot. You can hide here"),
-                     HelpItems(image: "joystick_background",secondImage: "joystick" , explenation: "This is an joystick. You can use this to move around on the map")]
+                     HelpItems(image: "Seeker", explenation: "This is a seeker. Your job as a seeker is to catch all other players."),
+                     HelpItems(image: "tentNew", explenation: "This is an hiding spot. You can hide here."),
+                     HelpItems(image: "tentOld", explenation: "This is an hiding spot. You can hide here."),
+                     HelpItems(image: "house", explenation: "This is an hiding spot. You can hide here."),
+                     HelpItems(image: "joystick_background",secondImage: "joystick" , explenation: "This is an joystick. You can use this to move around on the map."),
+                     HelpItems(image: "button", explenation: "This is the action button. I will have a label stating the action yo can preform.")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // This will add an cancel button in the nav bar.
         navBarItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(navBarTapped(sender:)))
         navBarItem.leftBarButtonItem?.tintColor = .systemRed
         tableview.delegate = self
         tableview.dataSource = self
     }
     
+    // This will handle the button click from a navigation bat item.
     @objc func navBarTapped(sender: UIBarButtonItem){
         switch sender {
         case navBarItem.leftBarButtonItem:
@@ -49,18 +53,22 @@ class HelpVC: UIViewController {
     }
 }
 
+// Here are the methodes needed to have a UITableView.
 extension HelpVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        helpItems.count
+        // This sets the number of rows needed for each section.
+        return helpItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // This will  create a cell.
         let cell = tableview.dequeueReusableCell(withIdentifier: "helpCell", for: indexPath) as! HelpCell
         cell.populate(helpItems[indexPath.row])
         return cell
     }
 
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // This sets the hight of the cell.
         return 125
     }
 }
